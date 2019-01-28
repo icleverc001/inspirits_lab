56f4cdd20414e0363838e9799d0a45e7b23beb20
icleverc001 <icleverc001@gmail.com>
2019-01-28 16:01:32 +0900

    second commit

--- exec_bottle.py
+++ exec_bottle.py
@@ -11,5 +11,5 @@ import main
 def exec():
     return main.exec_get_html()
 
-#run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
-run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
+#run(host='localhost', port=8080, debug=True)
